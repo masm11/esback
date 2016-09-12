f68cfcd6c4f82112a66d453770fa5ed39570cad6
Yuuki Harano <masm@masm11.ddo.jp>
2016-09-12 21:00:01 +0900

    backup now を preference にした。

---
+++ app/src/main/java/jp/ddo/masm11/esback/BackupNowPreference.java
@@ -0,0 +1,30 @@
+package jp.ddo.masm11.esback;
+
+import android.preference.Preference;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.util.AttributeSet;
+import android.app.AlertDialog;
+
+public class BackupNowPreference extends Preference {
+    public BackupNowPreference(Context context, AttributeSet attrs) {
+	super(context, attrs);
+    }
+    
+    @Override
+    protected void onClick() {
+	AlertDialog dialog = new AlertDialog.Builder(getContext())
+		.setMessage("Backup now?")
+		.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+		    public void onClick(DialogInterface dialog, int which) {
+		    }
+		})
+		.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+		    public void onClick(DialogInterface dialog, int which) {
+			// NOP
+		    }
+		})
+		.create();
+	dialog.show();
+    }
+}
